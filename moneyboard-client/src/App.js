9cad09606c673b07677fe59c40cb5454e86bda78
MVozniak <mikola.v.2016@gmail.com>
2023-10-05 17:41:06 +0300

    Routing improvement 1

--- moneyboard-client/src/App.js
+++ moneyboard-client/src/App.js
@@ -1,10 +1,26 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import Privacy from './pages/Privacy';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <>
-    <Header/>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route path='/' element={<Home />} />
+            <Route path='/privacy' element={<Privacy />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='*' element={<NotFound />} />
+          </Route>
+        </Routes>
+      </Router>
     </>
   );
 }
--- moneyboard-client/src/Components/Layout.js
+++ moneyboard-client/src/Components/Layout.js
@@ -1,22 +1,49 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Component } from 'react';
 import { Link, Outlet } from 'react-router-dom';
+import { Nav, Navbar } from 'react-bootstrap';
 
+import logo from '../resources/logo192.png';
 
 class Layout extends Component {
-    render() {
-        return (
-            <>
-                <header>
+  render() {
+    return (
+      <>
+        <header>
+          <Navbar expand='md' bg='dark' variant='dark' className='mx-0 px-3 border-bottom shadow-sm'>
+            <div className='container-fluid mx-0 px-0'>
+              <Navbar.Brand as={Link} to='/'>
+                <img
+                  src={logo}
+                  height='30'
+                  width='30'
+                  className='d-inline-block align-top mx-1'
+                  alt='MbLogo'
+                /> Moneyboard
+              </Navbar.Brand>
+              <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+              <Navbar.Collapse id='responsive-navbar-nav'>
+                <Nav className='col justify-content-start'>
+                  <Nav.Link as={Link} to='/'>Home</Nav.Link>
+                  <Nav.Link as={Link} to='/privacy'>Privacy</Nav.Link>
+                </Nav>
+                <Nav className='col justify-content-end'>
+                  <Nav.Link as={Link} to='/register'>Register</Nav.Link>
+                  <Nav.Link as={Link} to='/login'>Login</Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </div>
+          </Navbar>
+        </header>
 
-                </header>
+        <Outlet />
 
-                <Outlet/>
-
-                <footer>2023</footer>
-
-            </>
-        );
-    }
+        <footer>2023</footer>
+      </>
+    );
+  }
 }
 
 export default Layout;
+
+
