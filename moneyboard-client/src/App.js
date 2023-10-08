013f79395b2e2ba84d4b0d8324022558c257a0bd
MVozniak <mikola.v.2016@gmail.com>
2023-10-09 01:31:28 +0300

    created login user functional base

--- moneyboard-client/src/App.js
+++ moneyboard-client/src/App.js
@@ -6,6 +6,7 @@ import Privacy from './pages/Privacy';
 import Register from './pages/Register';
 import Login from './pages/Login';
 import NotFound from './pages/NotFound';
+import Officex from './pages/Officex';
 
 import axios from 'axios';
 
@@ -19,6 +20,7 @@ function App() {
             <Route path='/privacy' element={<Privacy />} />
             <Route path='/register' element={<Register />} />
             <Route path='/login' element={<Login />} />
+            <Route path='/office' element={<Officex />} />
             <Route path='*' element={<NotFound />} />
           </Route>
         </Routes>
--- moneyboard-client/src/pages/Home.js
+++ moneyboard-client/src/pages/Home.js
@@ -1,51 +1,52 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { Component } from 'react';
 import { Container, Row, Col, Carousel, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 import picture from '../resources/logo512.png';
+import { useAuth } from '../autentification/AuthContext';
 
-class Home extends Component {
-    render() {
-        return (
-            <>
-                <Container>
-                    <Row className='row align-items-center'>
-                        <Col xs={12} md={6}>
-                            <p className="fw-bold display-1">Moneyboard</p>
-                            <h4 className='mt-4'>The best solution for money management</h4>
-                            <Carousel controls={false}>
-                                <Carousel.Item>
-                                    <h4>Thesis 1</h4>
-                                </Carousel.Item>
-                                <Carousel.Item>
-                                    <h4>Thesis 2</h4>
-                                </Carousel.Item>
-                                <Carousel.Item>
-                                    <h4>Thesis 3</h4>
-                                </Carousel.Item>
-                            </Carousel>
-                            <div className='mt-4'>
-                                <Link to="/login">
-                                    <Button variant="success" className="btn-lg">Get started!</Button>
-                                </Link>
-                            </div>
-                        </Col>
-                        <Col xs={12} md={6}>
-                            <div className='d-flex justify-content-center mt-4'>
-                                <img
-                                    src={picture}
-                                    alt="picture"
-                                    width="90%"
-                                    className='d-flex justify-content-center'
-                                />
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
-            </>
-        );
-    }
+const Home = () => {
+
+    const { isLoggedIn } = useAuth();
+
+    return (
+        <>
+            <Container>
+                <Row className='row align-items-center'>
+                    <Col xs={12} md={6}>
+                        <p className="fw-bold display-1">Moneyboard</p>
+                        <h4 className='mt-4'>The best solution for money management</h4>
+                        <Carousel controls={false}>
+                            <Carousel.Item>
+                                <h4>Thesis 1</h4>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <h4>Thesis 2</h4>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <h4>Thesis 3</h4>
+                            </Carousel.Item>
+                        </Carousel>
+                        <div className='mt-4'>
+                            <Link to="/login">
+                                <Button variant="success" className="btn-lg">Get started!</Button>
+                            </Link>
+                        </div>
+                    </Col>
+                    <Col xs={12} md={6}>
+                        <div className='d-flex justify-content-center mt-4'>
+                            <img
+                                src={picture}
+                                alt="picture"
+                                width="90%"
+                                className='d-flex justify-content-center'
+                            />
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    );
 }
 
 export default Home;
