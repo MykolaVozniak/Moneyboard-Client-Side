1610d4752322ad98e89b8556d29c4dbaa38d5c4d
MVozniak <mikola.v.2016@gmail.com>
2023-10-09 01:08:14 +0300

    login page changes

--- moneyboard-client/src/pages/Login.js
+++ moneyboard-client/src/pages/Login.js
@@ -1,72 +1,67 @@
-import React, { Component } from 'react';
 import API_URL from '../config';
-//import { useAuth } from './autentification/AuthContext';
+import React, { useState } from 'react';
+import { useAuth } from '../autentification/AuthContext';
 
-class Login extends Component {
-  state = {
-    email: '',
-    password: ''
-  };
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-  handleChange = (e) => {
-    this.setState({ [e.target.name]: e.target.value });
-  };
+    const { login } = useAuth();
 
-  handleSubmit = async (e) => {
-    e.preventDefault();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
-    const url =  API_URL +  'api/Authentication/login';
+        const url = API_URL + 'api/Authentication/login';
 
-    try {
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(this.state)
-      });
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ email, password })
+            });
 
-      if (response.ok) {
-        alert('Ви успішно увійшли!');
-        // Додайте тут код для перенаправлення користувача на іншу сторінку, наприклад, після успішного входу
-      } else {
-        alert('Помилка входу. Будь ласка, перевірте ваш email та пароль.');
-      }
-    } catch (error) {
-      console.error('Помилка:', error);
-    }
-  };
+            if (response.ok) {
+                const data = await response.json();
+                const token = data.token; // Припустимо, що сервер надсилає токен як властивість 'token'
+                login(token);
+                alert('Ви успішно увійшли!');
+                // Додайте тут код для перенаправлення користувача на іншу сторінку, наприклад, після успішного входу
+            } else {
+                alert('Помилка входу. Будь ласка, перевірте ваш email та пароль.');
+            }
+        } catch (error) {
+            console.error('Помилка:', error);
+        }
+    };
 
-  render() {
     return (
-      <div>
-        <h2>Вхід</h2>
-        <form onSubmit={this.handleSubmit}>
-          <div>
-            <label>Email:</label>
-            <input
-              type="email"
-              name="email"
-              value={this.state.email}
-              onChange={this.handleChange}
-              required
-            />
-          </div>
-          <div>
-            <label>Пароль:</label>
-            <input
-              type="password"
-              name="password"
-              value={this.state.password}
-              onChange={this.handleChange}
-              required
-            />
-          </div>
-          <button type="submit">Увійти</button>
-        </form>
-      </div>
+        <div>
+            <h2>Вхід</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Email:</label>
+                    <input
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </div>
+                <div>
+                    <label>Пароль:</label>
+                    <input
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+                </div>
+                <button type="submit">Увійти</button>
+            </form>
+        </div>
     );
-  }
 }
 
 export default Login;
